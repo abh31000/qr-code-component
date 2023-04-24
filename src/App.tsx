bf7c690dc01a04c8ac1baa0d7631a38e47165bfc
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-04-24 23:31:51 +0100

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import QRCode from "./components/QRCode";
+
 export default function App() {
    return (
-      <h1 className='text-3xl font-bold underline'> Hello world! </h1>
+      <QRCode/>
    );
 }
